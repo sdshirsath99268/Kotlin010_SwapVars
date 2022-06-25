47199f0e0b455bbacf39df8d52b3f8458012c734
Sanjay Shirsath <sdshirsath@Sanjays-MacBook-Air.local>
2022-06-25 21:12:56 +0530

    Initial commit

---
+++ src/swap.kt
@@ -0,0 +1,19 @@
+fun main(args:Array<String>) {
+    var i = 10
+    var j = 20
+
+    println("Before Swap ......")
+    println("I = " +i)
+    println("J = " +j)
+
+    var tmp = i
+    i = j
+    j = tmp
+
+    println("After Swap ......")
+    println("I = " +i)
+    println("J = " +j)
+
+
+    println("No: 10 Kotline Prog , By Sanjay D Shirsath")
+}
